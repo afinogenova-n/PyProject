b5238c19fdab570ef321d37b1825afd946edcfa5
afinogenova <nast_af@mail.ru>
2021-03-06 22:47:29 +0300

    lesson_4

---
+++ lesson_4/dz_2.py
@@ -0,0 +1,22 @@
+import requests
+
+r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+
+def currency_rates():
+    ind_char_code = r.text.find(char_code.upper())
+    if ind_char_code == -1:
+        return None
+    ind_v = r.text[ind_char_code:].find('Value')
+    start = ind_v + 6
+    end = start + 7
+    value = r.text[ind_char_code:][start:end]
+    return f'Курс {char_code.upper()} {value}'
+
+char_code = input('Введите код валюты: ')
+print(currency_rates())
+
+
+
+
+
+
